4b939d1e246f6cbf3ea48c540a6acb542d02404a
Makhadmahaska <mahadmahaska@hotmail.com>
2025-04-08 13:29:29 +0200

    first initilize

---
+++ main.js
@@ -0,0 +1,74 @@
+/* 
+
+
+Instructions: 
+
+Create a small app to track books you’ve read or want to read (If you are not in to books, feel free to change it to movies). Use prompt and alert to simulate the menu. 
+
+You will: 
+
+Use objects to represent books 
+
+Store multiple books in an array 
+
+Write functions to add, list and update books 
+
+Requirements: 
+
+A library array to store all book objects 
+
+Each book should have: 
+
+Title 
+
+Author 
+
+isRead 
+
+Implement these functions: 
+
+addBook() – asks for details and adds a new book object to the library 
+
+listBooks() – display a list of all books (console.log works fine here) 
+
+markAsRead(title) – finds a book by title and sets isRead to true 
+
+ 
+
+I have created some starting code for you here: 
+
+En bild som visar text, skärmbild
+
+AI-genererat innehåll kan vara felaktigt., Picture 
+
+ 
+
+Bonus challenge (optional): 
+
+Allow the user to remove a book 
+
+Allow the user to list only unread books 
+
+ 
+
+These methods could be useful in this assigment so you’ll have to do some research on your own: 
+
+push( ) 
+
+forEach( ) 
+
+find( ) 
+
+filter( ) 
+
+ 
+
+Paste your Github link here: 
+
+ 
+
+Feedback (will be filled out by teacher/ teacher assistant): 
+
+ */
+
+
